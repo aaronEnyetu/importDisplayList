27628de762ec1932319091895f03baba612b3ba2
Aaron Enyetu <aaronenyetu@gmail.com>
2022-05-06 12:57:59 -0700

    imported modules under test and set the expected and actual variables in the test function

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,21 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { renderPlant } from '../utils.js';
+import { plants } from '../plants.js';
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('test plant render function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = `<div class="plant"><h1>corn</h1><img src="./assets/corn.jpg"><p>corn is a cereal grain first domesticated in mexico about 10000BC years ago  </p><h2>Types</h2><ul><li>sweetcorn</li><li>flintcorn</li><li>popcorn</li></ul></div>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderPlant(plants[0]);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected);
 });
